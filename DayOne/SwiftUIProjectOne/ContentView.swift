2fc05b82f3a78db77c86f58c249b00cb468cbeff
rohshar623 <rohshar623@gmail.com>
2023-01-29 16:27:36 +0530

    state properties

--- DayOne/SwiftUIProjectOne/ContentView.swift
+++ DayOne/SwiftUIProjectOne/ContentView.swift
@@ -7,23 +7,37 @@
 
 import SwiftUI
 
+/*
+var body: some View {
+    NavigationStack {
+        NavigationLink {
+            Text("destination")
+        } label: {
+            Text("Tap Me")
+        }
+        Form {
+            Section {
+                Text("hello world!")
+                Text("hello world!")
+                Text("hello world!")
+            }
+        }
+    }.navigationBarTitleDisplayMode(.inline)
+    
+}
+*/
+
+// State helps in modifying in the struct...
 struct ContentView: View {
+    @State private var tapCount = 0
     var body: some View {
-        NavigationStack {
-            NavigationLink {
-                Text("destination")
-            } label: {
-                Text("Tap Me")
-            }
-            Form {
-                Section {
-                    Text("hello world!")
-                    Text("hello world!")
-                    Text("hello world!")
-                }
-            }
-        }.navigationBarTitleDisplayMode(.inline)
-        
+        Button("Tap count: \(tapCount)") {
+            self.tapCount += 1
+        }
+    }
+    
+    mutating func incrementCount() {
+        tapCount += 1
     }
 }
 
