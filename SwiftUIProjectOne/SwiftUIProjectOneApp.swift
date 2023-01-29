3214308e4bbb2586caf620b7d2fabe5822d5b075
rohshar623 <rohshar623@gmail.com>
2023-01-29 15:22:35 +0530

    Initial Commit

---
+++ SwiftUIProjectOne/SwiftUIProjectOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectOneApp.swift
+//  SwiftUIProjectOne
+//
+//  Created by Rohit Sharma on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
