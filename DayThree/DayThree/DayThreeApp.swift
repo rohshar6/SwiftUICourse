d1dd8b604c4ccc5a50d366befb03c25192204586
rohshar623 <rohshar623@gmail.com>
2023-01-31 22:02:19 +0530

    day 3

---
+++ DayThree/DayThree/DayThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayThreeApp.swift
+//  DayThree
+//
+//  Created by Rohit Sharma on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DayThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
