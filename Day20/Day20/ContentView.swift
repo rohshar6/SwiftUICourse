b82d798c623b6a4eb6f7da28791422d0a9a88c65
rohshar623 <rohshar623@gmail.com>
2023-02-02 23:44:08 +0530

    added day-20

---
+++ Day20/Day20/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Day20
+//
+//  Created by Rohit Sharma on 02/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day20/Day20/Day20App.swift
@@ -0,0 +1,17 @@
+//
+//  Day20App.swift
+//  Day20
+//
+//  Created by Rohit Sharma on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Day20App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
